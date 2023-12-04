9464159f237c76a4ef7868203806f1a608807aa9
dhsin98 <shinjay417@gmail.com>
2023-12-04 15:54:36 +0900

    feat: chatroom Local에 저장

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,16 @@
 import { create } from "zustand";
 
-const useStore = create((set) => ({
-  chatRooms: [],
+const useStore = create((set, get) => ({
+  chatRooms: JSON.parse(localStorage.getItem("chatRooms")) || [],
   chatId: null,
   pdfName: "",
+  chatRoomsRecord: [],
   addNewChatRoom: (id, name) =>
     set((state) => {
       const newChatRoom = { id, name: name || "New ChatRoom" };
+      const updatedChatRooms = [...get().chatRooms, newChatRoom];
+      localStorage.setItem("chatRooms", JSON.stringify(updatedChatRooms));
+      set({ chatRooms: updatedChatRooms, chatId: id });
       return { chatRooms: [...state.chatRooms, newChatRoom], chatId: id };
     }),
   setChatId: (id) => set({ chatId: id }),
